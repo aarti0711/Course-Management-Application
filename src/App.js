d27cb0f7f4d6d765aca5fda50a80a4ed9dc9891c
aarti0711 <90836137+aarti0711@users.noreply.github.com>
2021-10-04 12:21:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ const App = () => {
         <Switch>
           <Route exact path={["/", "/home"]} component={Home} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+          <Route exact path="/signup" component={Register} />
           <Route exact path="/profile" component={Profile} />
           <Route path="/add" component={AddCourse} />
           <Route path="/list" component={AllCourses} />
